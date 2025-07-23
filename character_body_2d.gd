e4dbb73fcb8f18db4cf0e9f458213f19cf6acc38
Devansh365 <devanshsaini365@gmail.com>
2025-07-23 18:10:41 +0530

    player death anim

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,6 +5,7 @@ var bullet_scene = preload("res://bullet1.tscn")
 var bullet_scene3 = preload("res://bullet3.tscn")
 var bullet_scene2 = preload("res://bullet_2.tscn")
 var weapon = 1
+var margya = false
 
 const SPEED = 300.0
 signal healthchanged
@@ -25,12 +26,19 @@ func _ready() -> void:
 	heal_timer.autostart = true
 	add_child(heal_timer)
 	heal_timer.timeout.connect(_on_heal_timer_timeout)
+	margya = false
 
 func _physics_process(delta: float) -> void:
 	healthchanged.emit()
 	manachanged.emit()
 	if player_health <= 0:
-		get_tree().reload_current_scene()
+		margya = true
+		if margya == true:
+			$AnimationPlayer.play("playerdeath")
+			await get_tree().create_timer(3.0).timeout
+			get_tree().reload_current_scene()
+		
+		
 
 	look_at(get_global_mouse_position())
 
@@ -43,28 +51,31 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		if weapon == 1:
 			if mana >= 20:
-				var bullet = bullet_scene.instantiate()
-				bullet.global_position = bullethole.global_position
-				bullet.direction = (get_global_mouse_position() - global_position).normalized()
-				get_tree().current_scene.add_child(bullet)
+				if margya == false:
+					var bullet = bullet_scene.instantiate()
+					bullet.global_position = bullethole.global_position
+					bullet.direction = (get_global_mouse_position() - global_position).normalized()
+					get_tree().current_scene.add_child(bullet)
 
-				mana -= 20
-				
+					mana -= 20
+					
 		if weapon == 2:
 			if mana >= 40:
-				var bullet2 = bullet_scene2.instantiate()
-				bullet2.global_position = bullethole.global_position
-				bullet2.direction = (get_global_mouse_position() - global_position).normalized()
-				get_tree().current_scene.add_child(bullet2)
-				mana -= 40
+				if margya == false:
+					var bullet2 = bullet_scene2.instantiate()
+					bullet2.global_position = bullethole.global_position
+					bullet2.direction = (get_global_mouse_position() - global_position).normalized()
+					get_tree().current_scene.add_child(bullet2)
+					mana -= 40
 		if weapon == 3:
 			if mana >= 75:
-				var bullet3 = bullet_scene3.instantiate()
-				bullet3.global_position = bullethole.global_position
-				bullet3.direction = (get_global_mouse_position() - global_position).normalized()
-				get_tree().current_scene.add_child(bullet3)
-				mana -= 75
-	
+				if margya == false:
+					var bullet3 = bullet_scene3.instantiate()
+					bullet3.global_position = bullethole.global_position
+					bullet3.direction = (get_global_mouse_position() - global_position).normalized()
+					get_tree().current_scene.add_child(bullet3)
+					mana -= 75
+		
 
 	mana_regen_timer += delta
 	if mana < max_mana and mana_regen_timer >= 0.1:
@@ -83,14 +94,17 @@ func _physics_process(delta: float) -> void:
 	player_health = clamp(player_health, 0, max_health)
 	
 func playerbullet1hit():
-	player_health = player_health - 10
-	$ouch.play()
-	$AnimationPlayer.play("damagetaken")
+	if margya == false:
+		player_health = player_health - 10
+		$ouch.play()
+		$AnimationPlayer.play("damagetaken")
 func playerspiralbullethit():
-	player_health = player_health - 2
-	$ouch.play()
-	$AnimationPlayer.play("damagetaken")
+	if margya == false:
+		player_health = player_health - 2
+		$ouch.play()
+		$AnimationPlayer.play("damagetaken")
 func _on_heal_timer_timeout() -> void:
-	if player_health < max_health:
-		player_health = min(player_health + 1, max_health)
-		emit_signal("healthchanged")
+	if margya == false:
+		if player_health < max_health:
+			player_health = min(player_health + 1, max_health)
+			emit_signal("healthchanged")
